5cb43cf647b0d35228e243e51c21d1e8eb580511
David Lilienthal <david.lilienthal@gmail.com>
2012-06-21 22:16:55 +0100

    Missed this

---
+++ Raita.Web/Content/editor.js
@@ -0,0 +1,22 @@
+﻿require(['jquery', 'gherkin-editor'], function ($) {
+	function callback(editor) {
+		require(['ace/theme-twilight'], function () {
+			editor.setTheme("ace/theme/twilight");
+			editor.getSession().setTabSize(2);
+			editor.getSession().setUseSoftTabs(true);
+		});
+	}
+
+	$('#editor').gherkinEditor({
+		callback: callback,
+		stepdefs: {
+			'I make a syntax error': [],
+			'I have (\\d+) cukes in my belly': [],
+			'I have eaten all the cukes': [],
+			'^stuff should be (.*)':
+				['stuff should be blue',
+				'stuff should be green',
+				'stuff should be red']
+		}
+	});
+});
