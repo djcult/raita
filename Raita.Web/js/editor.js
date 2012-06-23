ed1543b612118e6f96c4691dd7c620ffc9a7f405
David Lilienthal <david.lilienthal@gmail.com>
2012-06-23 17:23:38 +0100

    Missed in previous commit

--- Raita.Web/js/editor.js
+++ Raita.Web/js/editor.js
@@ -1,22 +1,24 @@
 ﻿require(['jquery', 'gherkin-editor'], function ($) {
-	function callback(editor) {
-		require(['ace/theme-twilight'], function () {
-			editor.setTheme("ace/theme/twilight");
-			editor.getSession().setTabSize(2);
-			editor.getSession().setUseSoftTabs(true);
-		});
-	}
+    function callback(editor) {
+        require(['ace/theme-twilight'], function () {
 
-	$('#editor').gherkinEditor({
-		callback: callback,
-		stepdefs: {
-			'I make a syntax error': [],
-			'I have (\\d+) cukes in my belly': [],
-			'I have eaten all the cukes': [],
-			'^stuff should be (.*)':
+            editor.setTheme("ace/theme/twilight");
+            editor.getSession().setTabSize(2);
+            editor.getSession().setUseSoftTabs(true);
+            editor.renderer.setShowPrintMargin(false);
+        });
+    }
+
+    $('#editor').gherkinEditor({
+        callback: callback,
+        stepdefs: {
+            'I make a syntax error': [],
+            'I have (\\d+) cukes in my belly': [],
+            'I have eaten all the cukes': [],
+            '^stuff should be (.*)':
 				['stuff should be blue',
 				'stuff should be green',
 				'stuff should be red']
-		}
-	});
+        }
+    });
 });
